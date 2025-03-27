d79cce806b0fc5bdb649175fb9cccb08478326b6
Lenin Meza <merolhack@gmail.com>
2025-03-26 20:21:24 -0600

    :lipstick: Video version 2

--- src/components/Dashboard/Main/Video.tsx
+++ src/components/Dashboard/Main/Video.tsx
@@ -6,14 +6,12 @@ import { Card } from "react-bootstrap";
 const Video = () => {
     return (
       <>
-        <Card className="">
-          <div className="d-flex justify-content-between align-items-center flex-wrap">
-            <video width="320" height="180" autoPlay muted loop preload="auto">
-                <source src="/videos/chambiar-avatar-001.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-          </div>
-        </Card>
+        <div className="d-flex justify-content-between align-items-center flex-wrap">
+          <video width="320" height="260" autoPlay muted loop preload="auto">
+              <source src="/videos/chambiar-avatar-002.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+          </video>
+        </div>
       </>
     );
   };
