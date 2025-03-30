ac13ff9e2d5cceacbb2df9e572742717ffd5c0e3
Lenin Meza <merolhack@gmail.com>
2025-03-30 07:17:15 -0600

    :lipstick: Calendar events

--- src/components/Dashboard/Main/WorkingSchedule/UpcomingEvents/index.js
+++ src/components/Dashboard/Main/WorkingSchedule/UpcomingEvents/index.js
@@ -5,22 +5,34 @@ import { Card } from "react-bootstrap";
 const UpcomingEvents = () => {
   return (
     <>
-      <div className="upcoming-events mt-4 position-relative">
+      <div className="upcoming-events position-relative">
         <Card className="bg-white border-0 rounded-3 mb-4">
           <Card.Body className="p-4">
             <div className="mb-3 mb-lg-4">
-              <h3 className="mb-0">Upcoming Events</h3>
+
             </div>
 
             <div className="upcoming-events-list">
-              <div className="event-item">
-                <h5>Project Deadline</h5>
-                <p>Due: 2025-05-01</p>
-              </div>
-              <div className="event-item">
-                <h5>Team Meeting</h5>
-                <p>Date: 2025-05-03</p>
-              </div>
+              <Card className="bg-chambiar-bg border-0 rounded-3 mb-4">
+                <Card.Body className="p-4">
+                  <div className="d-flex align-items-center mb-3">
+                    <h4 className="fs-18 fw-semibold text-dark">10:00 AM</h4>
+                  </div>
+                  <p className="text-secondary fs-15 mb-0">
+                    Product sync
+                  </p>
+                </Card.Body>
+              </Card>
+              <Card className="bg-chambiar-bg border-0 rounded-3 mb-4">
+                <Card.Body className="p-4">
+                  <div className="d-flex align-items-center mb-3">
+                    <h4 className="fs-18 fw-semibold text-dark">1:00 PM</h4>
+                  </div>
+                  <p className="text-secondary fs-15 mb-0">
+                    Vendor review
+                  </p>
+                </Card.Body>
+              </Card>
             </div>
           </Card.Body>
         </Card>
